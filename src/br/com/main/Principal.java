65e5fd8cdbf1c1d8e0f1d97400d781cd4b2a95e9
AdailtonS <adailton.silva29@etec.sp.gov.br>
2019-10-24 20:21:55 -0300

    Criando o Programa

---
+++ src/br/com/main/Principal.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package br.com.main;
+
+import java.util.Scanner;
+
+/**
+ * @author AJS
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		double peso;
+		double altura;
+		
+		
+		
+		System.out.println("Qual o seu peso: ");
+		peso  = scanner.nextDouble();
+		
+		System.out.println("Qual é a sua altura? ");
+		altura = scanner.nextDouble();
+		
+		Calcular calcular = new Calcular();
+		System.out.println("Resultado: " + calcular.calcularImc(peso, altura));
+		
+		
+	}
+
+}
