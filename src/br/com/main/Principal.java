e8eed2114a0975464ae30ef68fedb6765b466777
AdailtonS <adailton.silva29@etec.sp.gov.br>
2019-10-26 16:03:48 -0300

    Atualizando o Programa

--- src/br/com/main/Principal.java
+++ src/br/com/main/Principal.java
@@ -31,6 +31,20 @@ public class Principal {
 		Calcular calcular = new Calcular();
 		System.out.println("Resultado: " + calcular.calcularImc(peso, altura));
 		
+		//double resultado;
+		//resultado = peso / (altura * 2);
+				
+		if (peso / (altura * altura) < 20) {
+			System.out.println("Magro.");
+		}else if (peso / (altura * altura) >= 20 && peso / (altura * altura) <= 24) {
+			System.out.println("Normal.");
+		}else if (peso / (altura * altura) >= 25 && peso / (altura * altura) <= 29) {
+			System.out.println("Acima do peso.");
+		}else if (peso / (altura * altura) >= 30 && peso / (altura * altura) <= 34) {
+			System.out.println("Obeso.");
+		}else {
+			System.out.println("Muito Obeso.");
+		}
 		
 	}
 
