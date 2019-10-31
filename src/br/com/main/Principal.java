f26b4ac821f30d0f0f6ca84c6122a811e22b675b
AdailtonS <adailton.silva29@etec.sp.gov.br>
2019-10-31 16:49:46 -0300

    Fazendo o programa funcionar

--- src/br/com/main/Principal.java
+++ src/br/com/main/Principal.java
@@ -32,15 +32,15 @@ public class Principal {
 		System.out.println("Resultado: " + calcular.calcularImc(peso, altura));
 		
 		double resultado;
-		resultado = peso / (altura * 2);
+		resultado = peso / (altura * altura);
 						
-		if resultado < 20) {
+		if (resultado < 20) {
 			System.out.println("Magro.");
-		}else if resultado >= 20 && resultado <= 24) {
+		}else if (resultado >= 20 && resultado <= 24) {
 			System.out.println("Normal.");
-		}else if resultado >= 25 && resultado <= 29) {
+		}else if (resultado > 24 && resultado <= 29) {
 			System.out.println("Acima do peso.");
-		}else if resultado >= 30 && resultado <= 34) {
+		}else if (resultado > 29 && resultado <= 34) {
 			System.out.println("Obeso.");
 		}else {
 			System.out.println("Muito Obeso.");
