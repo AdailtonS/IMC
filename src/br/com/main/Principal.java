c39a73a74d0c4291bf09cbecda06b3cfed049435
AdailtonS <54690806+AdailtonS@users.noreply.github.com>
2019-10-31 16:34:05 -0300

    Atualizando para variável resultado

--- src/br/com/main/Principal.java
+++ src/br/com/main/Principal.java
@@ -25,22 +25,22 @@ public class Principal {
 		System.out.println("Qual o seu peso: ");
 		peso  = scanner.nextDouble();
 		
-		System.out.println("Qual é a sua altura? ");
+		System.out.println("Qual Ã© a sua altura? ");
 		altura = scanner.nextDouble();
 		
 		Calcular calcular = new Calcular();
 		System.out.println("Resultado: " + calcular.calcularImc(peso, altura));
 		
-		//double resultado;
-		//resultado = peso / (altura * 2);
+		double resultado;
+		resultado = peso / (altura * 2);
 						
-		if (peso / (altura * altura) < 20) {
+		if resultado < 20) {
 			System.out.println("Magro.");
-		}else if (peso / (altura * altura) >= 20 && peso / (altura * altura) <= 24) {
+		}else if resultado >= 20 && resultado <= 24) {
 			System.out.println("Normal.");
-		}else if (peso / (altura * altura) >= 25 && peso / (altura * altura) <= 29) {
+		}else if resultado >= 25 && resultado <= 29) {
 			System.out.println("Acima do peso.");
-		}else if (peso / (altura * altura) >= 30 && peso / (altura * altura) <= 34) {
+		}else if resultado >= 30 && resultado <= 34) {
 			System.out.println("Obeso.");
 		}else {
 			System.out.println("Muito Obeso.");
